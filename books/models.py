877be76b903b897fb2ed2c8bac642ed8ba962759
ultra-hash <jarviscommand3@gmail.com>
2022-09-21 15:17:01 +0000

    model created and create view in books app create

--- books/models.py
+++ books/models.py
@@ -1,3 +1,4 @@
 from django.db import models
 
-# Create your models here.
+class books(models.Model):
+    title = models.CharField(max_length=40)
--- books/views.py
+++ books/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import books
+
 # Create your views here.
 
 def index(request):
     return HttpResponse('Hello From Books')
+
+
+def create(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        book = books.objects.create(title=title)
+        return redirect("books.index")
+    else:
+        return render(request, "books/books_form.html", {})
+
+def update(request):
+    return
+
+def delete(request):
+    return
+
+def list(request):
+    return
+    
