519e0b0c70582ae0df2f9ea536a80c8d9b08de0c
ultra-hash <jarviscommand3@gmail.com>
2022-09-21 17:03:22 +0000

    added static files  directory and configered in settings.py along with tempaltes Dirs

--- LMS/settings.py
+++ LMS/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'LMS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'static/templates/'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,6 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
