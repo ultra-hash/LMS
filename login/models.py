126d2cbf7c2f4feeb02f0943a36760eb36ea4578
ultra-hash <jarviscommand3@gmail.com>
2022-09-21 16:07:15 +0000

    created adminAccounts model and migrated succesfully

--- login/models.py
+++ login/models.py
@@ -1,3 +1,8 @@
+import email
 from django.db import models
 
-# Create your models here.
+class adminAccounts(models.Model):
+    username = models.CharField(max_length=20)
+    email = models.EmailField(max_length=256)
+    password = models.CharField(max_length=256)
+    
--- login/views.py
+++ login/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import adminAccounts
 # Create your views here.
 
 def login(request):
